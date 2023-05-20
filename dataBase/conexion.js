89a43ef0471b94e17d22cabcada10005f558a256
ARGEEK1 <antonio198889@gmail.com>
2023-05-20 21:58:11 +0200

    update

--- dataBase/conexion.js
+++ dataBase/conexion.js
@@ -1,13 +1,16 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_PORT, DB_URL } = process.env;
+const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD } = process.env;
 
 var conexion = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 conexion.connect((err) => {
