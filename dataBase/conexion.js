c3b20b27c8a036ba87f33f555434c4f90b0973c1
ARGEEK1 <antonio198889@gmail.com>
2023-05-20 19:24:57 +0200

    update

--- dataBase/conexion.js
+++ dataBase/conexion.js
@@ -5,7 +5,6 @@ const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_PORT, DB_URL } = process.env;
 
 var conexion = mysql.createConnection({
   host: DB_HOST,
-  port: DB_PORT,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
