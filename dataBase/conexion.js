d62c8c027acc9660c6c32f7ffd12665e9697204a
ARGEEK1 <antonio198889@gmail.com>
2023-05-20 19:07:03 +0200

    update

--- dataBase/conexion.js
+++ dataBase/conexion.js
@@ -1,25 +1,22 @@
 var mysql = require("mysql");
-require('dotenv').config();
+require("dotenv").config();
 
-const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_PORT, DB_URL} = process.env;
+const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, DB_PORT, DB_URL } = process.env;
 
-var conexion = mysql.createConnection(DB_URL)
-//    {
-//     host: DB_HOST,
-//     port: DB_PORT,
-//     user: DB_USER,
-//     password: DB_PASSWORD,
-//     database: DB_NAME,
-// });
+var conexion = mysql.createConnection({
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+});
 
-conexion.connect(
-    (err)=>{
-        if(!err){
-            console.log("Conexión Establecida");
-        }else{
-            console.log("Error de Conexión");
-        }
-    }
-);
+conexion.connect((err) => {
+  if (!err) {
+    console.log("Conexión Establecida");
+  } else {
+    console.log("Error de Conexión");
+  }
+});
 
 module.exports = conexion;
